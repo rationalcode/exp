40ba24d3f6319f15793fb631d802262ccb13e1d6
rationalcode <marat_pochta@mail.ru>
2017-06-07 00:38:33 +0500

    add toast

--- app/src/main/java/com/example/admin/exp/MainSelect.java
+++ app/src/main/java/com/example/admin/exp/MainSelect.java
@@ -1,13 +1,46 @@
 package com.example.admin.exp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainSelect extends Activity {
+public class MainSelect extends Activity implements View.OnClickListener {
+
+
+    public static EditText printText;
+    public static Button buttonClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_select);
+
+    printText = (EditText) findViewById(R.id.editText);
+        buttonClick = (Button) findViewById(R.id.button);
+        buttonClick.setOnClickListener(this);
+
+
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId()==R.id.button) {
+
+            Toast toast = Toast.makeText(getApplicationContext(), "click Button", Toast.LENGTH_LONG);
+            toast.show();
+
+            printText.setText("Нажата кнопка");
+        }
+    }
+
+
+
 }
