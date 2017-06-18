15bbcf561f2ffcc049ee18761f182c8f325e54b4
rationalcode <marat_pochta@mail.ru>
2017-06-18 22:23:01 +0500

    add method onSaveInstanceState

--- app/src/main/java/com/example/admin/exp/MainSelect.java
+++ app/src/main/java/com/example/admin/exp/MainSelect.java
@@ -7,6 +7,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainSelect extends Activity implements View.OnClickListener {
@@ -14,17 +15,36 @@ public class MainSelect extends Activity implements View.OnClickListener {
 
     public static EditText printText;
     public static Button buttonClick;
+    public static TextView textView;
+
+    public int count = 0;
+
+    public boolean newState;
+    public boolean oldState;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_select);
 
-    printText = (EditText) findViewById(R.id.editText);
+        printText = (EditText) findViewById(R.id.editText);
         buttonClick = (Button) findViewById(R.id.button);
         buttonClick.setOnClickListener(this);
 
+        textView = (TextView) findViewById(R.id.textView);
 
+
+
+        if (savedInstanceState!=null){
+
+            count = savedInstanceState.getInt("count");
+            newState = savedInstanceState.getBoolean("newState");
+            oldState = savedInstanceState.getBoolean("oldState");
+
+        }
+
+        textView.setText(Integer.toString(count));
 
     }
 
@@ -34,13 +54,26 @@ public class MainSelect extends Activity implements View.OnClickListener {
 
         if (v.getId()==R.id.button) {
 
+            count++;
+
             Toast toast = Toast.makeText(getApplicationContext(), "click Button", Toast.LENGTH_LONG);
             toast.show();
 
             printText.setText("Нажата кнопка");
+            textView.setText(Integer.toString(count));
+
         }
     }
 
+
+    @Override
+    public void onSaveInstanceState (Bundle savedInstanceState){
+
+        savedInstanceState.putInt("count", count);
+        //savedInstanceState.putBoolean("newState", newState);
+        //savedInstanceState.putBoolean("oldState", oldState);
+
+    }
 
 
 }
